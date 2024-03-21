30364c7a29d48c6172cbe05495d751d157f6b860
Mitchel Sellers <msellers@Iowacomputergurus.com>
2024-03-20 22:36:48 -0500

    Fixes #34

--- README.md
+++ README.md
@@ -6,7 +6,9 @@
 
 This is a base library to provide utilities for working with email in .NET 6.  This project is used by more concrete implementations such as NetCore.Utilities.Email.Smtp.
 
-## Usage
+## Breaking Changes (Version 7.0)
+
+Starting with version 7.0, all `IEmailService` methods were converted to Async!
 
 ## Installation
 Standard installation via NuGet Package Manager
--- src/NetCore.Utilities.Email/EmailTemplateFactory.cs
+++ src/NetCore.Utilities.Email/EmailTemplateFactory.cs
@@ -4,72 +4,76 @@ using System.Text;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 
-namespace ICG.NetCore.Utilities.Email
+namespace ICG.NetCore.Utilities.Email;
+
+/// <summary>
+///     Factory class for creating email content, using templates.
+/// </summary>
+public interface IEmailTemplateFactory
 {
     /// <summary>
-    ///     Factory class for creating email content, using templates.
+    ///     Creates HTML email content utilizing a template
     /// </summary>
-    public interface IEmailTemplateFactory
+    /// <param name="subject">The desired subject of the email</param>
+    /// <param name="content">The desired content of the email, HTML formatted.</param>
+    /// <param name="preview">
+    ///     An optional preview textual email content element, replaced in the template for more advanced
+    ///     control
+    /// </param>
+    /// <param name="templateName">The name of the template to use, rather than the initial template.</param>
+    /// <returns>The updated content wrapped in the template</returns>
+    /// <exception cref="ArgumentNullException">Thrown for missing subject or content</exception>
+    /// <exception cref="ArgumentException">Thrown if requesting a template that is not defined</exception>
+    /// <exception cred="FileNotFoundException">Thrown if the defined template file does not exist</exception>
+    string BuildEmailContent(string subject, string content, string preview = "", string templateName = "");
+}
+
+/// <inheritdoc cref="IEmailTemplateFactory" />
+public class EmailTemplateFactory : IEmailTemplateFactory
+{
+    private readonly IHostEnvironment _hostingEnvironment;
+    private readonly IOptions<EmailTemplateSettings> _templateSettings;
+
+    /// <summary>
+    ///     Default constructor with injected dependencies
+    /// </summary>
+    /// <param name="templateSettings"></param>
+    /// <param name="hostingEnvironment"></param>
+    public EmailTemplateFactory(IOptions<EmailTemplateSettings> templateSettings, IHostEnvironment hostingEnvironment)
     {
-        /// <summary>
-        ///     Creates HTML email content utilizing a template
-        /// </summary>
-        /// <param name="subject">The desired subject of the email</param>
-        /// <param name="content">The desired content of the email, HTML formatted.</param>
-        /// <param name="preview">
-        ///     An optional preview textual email content element, replaced in the template for more advanced
-        ///     control
-        /// </param>
-        /// <param name="templateName">The name of the template to use, rather than the initial template.</param>
-        /// <returns>The updated content wrapped in the template</returns>
-        /// <exception cref="ArgumentNullException">Thrown for missing subject or content</exception>
-        /// <exception cref="ArgumentException">Thrown if requesting a template that is not defined</exception>
-        /// <exception cred="FileNotFoundException">Thrown if the defined template file does not exist</exception>
-        string BuildEmailContent(string subject, string content, string preview = "", string templateName = "");
+        _templateSettings = templateSettings;
+        _hostingEnvironment = hostingEnvironment;
     }
 
-
-    public class EmailTemplateFactory : IEmailTemplateFactory
+    /// <inheritdoc cref="IEmailTemplateFactory" />
+    public string BuildEmailContent(string subject, string content, string preview = "", string templateName = "")
     {
-        private readonly IHostEnvironment _hostingEnvironment;
-        private readonly IOptions<EmailTemplateSettings> _templateSettings;
+        //Validate inputs
+        if (string.IsNullOrEmpty(subject))
+            throw new ArgumentNullException(nameof(subject));
+        if (string.IsNullOrEmpty(content))
+            throw new ArgumentNullException(nameof(content));
+        if (!string.IsNullOrEmpty(templateName) &&
+            !_templateSettings.Value.AdditionalTemplates.ContainsKey(templateName))
+            throw new ArgumentException($"Requested template {templateName} was not found in configuration",
+                nameof(templateName));
 
-        public EmailTemplateFactory(IOptions<EmailTemplateSettings> templateSettings,
-            IHostEnvironment hostingEnvironment)
-        {
-            _templateSettings = templateSettings;
-            _hostingEnvironment = hostingEnvironment;
-        }
+        //Get the template
+        var templatePath = string.IsNullOrEmpty(templateName)
+            ? _templateSettings.Value.DefaultTemplatePath
+            : _templateSettings.Value.AdditionalTemplates[templateName];
+        var fullTemplatePath = Path.Combine(_hostingEnvironment.ContentRootPath, templatePath);
+        if (!File.Exists(fullTemplatePath))
+            throw new FileNotFoundException("Unable to find template file", fullTemplatePath);
 
-        public string BuildEmailContent(string subject, string content, string preview = "", string templateName = "")
-        {
-            //Validate inputs
-            if (string.IsNullOrEmpty(subject))
-                throw new ArgumentNullException(nameof(subject));
-            if (string.IsNullOrEmpty(content))
-                throw new ArgumentNullException(nameof(content));
-            if (!string.IsNullOrEmpty(templateName) &&
-                !_templateSettings.Value.AdditionalTemplates.ContainsKey(templateName))
-                throw new ArgumentException($"Requested template {templateName} was not found in configuration",
-                    nameof(templateName));
+        //Replace the content
+        var templateBuilder =
+            new StringBuilder(File.ReadAllText(fullTemplatePath));
+        templateBuilder.Replace("[SUBJECT]", subject);
+        templateBuilder.Replace("[PREVIEW]", preview);
+        templateBuilder.Replace("[CONTENT]", content);
 
-            //Get the template
-            var templatePath = string.IsNullOrEmpty(templateName)
-                ? _templateSettings.Value.DefaultTemplatePath
-                : _templateSettings.Value.AdditionalTemplates[templateName];
-            var fullTemplatePath = Path.Combine(_hostingEnvironment.ContentRootPath, templatePath);
-            if (!File.Exists(fullTemplatePath))
-                throw new FileNotFoundException("Unable to find template file", fullTemplatePath);
-
-            //Replace the content
-            var templateBuilder =
-                new StringBuilder(File.ReadAllText(Path.Combine(_hostingEnvironment.ContentRootPath, templatePath)));
-            templateBuilder.Replace("[SUBJECT]", subject);
-            templateBuilder.Replace("[PREVIEW]", preview);
-            templateBuilder.Replace("[CONTENT]", content);
-
-            //Return message content
-            return templateBuilder.ToString();
-        }
+        //Return message content
+        return templateBuilder.ToString();
     }
 }
